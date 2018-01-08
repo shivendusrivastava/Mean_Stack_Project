ffa0f7aef76819fa584baca1840b95020cea41a7
Anand <ankit.d.anand@accenture.com>
2018-01-08 14:28:11 +0530

    changed app component

--- shiv world/mean_stack_project/mean/src/app/app.component.ts
+++ shiv world/mean_stack_project/mean/src/app/app.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
 
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'app';  
+// }
+import {Component} from '@angular/core';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+selector:'app-root',
+template:`<div>
+<h1>{{pageTitle}}</h1>
+</div><br/>`
+
 })
-export class AppComponent {
-  title = 'app';
-  //added by Ankit;
+
+export class AppComponent{
+pageTitle: string = 'Shiv World Application';
+
 }
