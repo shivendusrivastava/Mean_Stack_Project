a679f0d6ddf8bf5fc229c3ddb95e7c67978c00fd
Shivendu <shivendu.sri@gmail.com>
2018-01-17 21:37:02 +0530

    Completed Angular Reactive Form

--- shiv world/mean_stack_project/mean/src/app/AngularForm/angularForm.component.ts
+++ shiv world/mean_stack_project/mean/src/app/AngularForm/angularForm.component.ts
@@ -1,6 +1,10 @@
 import {Component} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
+
+//Tutorial Link
+//https://www.youtube.com/watch?v=bo1Wu0aiigU
+
 @Component({
     selector:'app-form',
     templateUrl:'./angularForm.component.html',
@@ -13,17 +17,34 @@ export class AngularFormComponent{
     post: any;
     description: string = '';
     name: string = '';
+    titleAlert:string = 'This field is required';   
 
     constructor(private fb:FormBuilder){
         this.rForm = fb.group({
-            'name':[null, Validators.required],
-            'description':[null, Validators.compose([Validators.required, Validators.minLength(10), Validators.maxLength(500)])],
-            'validate': ''
+            'name' : [null, Validators.required],
+            'description' : [null, Validators.compose([Validators.required, Validators.minLength(30), Validators.maxLength(500)])],
+            'validate' : ''
         });
     }
-
+    
     postData(post){
         this.description = post.description;
         this.name = post.name;
     }
+
+    ngOnInit() {       
+        this.rForm.get('validate').valueChanges.subscribe(
+            (validate) => {
+                if (validate == '1'){
+                    this.rForm.get('name').setValidators([Validators.required, Validators.minLength(3)]);
+                    this.titleAlert = 'You need to specify atleast 3 characters';
+                }
+                else{
+                    this.rForm.get('name').setValidators(Validators.required);
+                }
+                this.rForm.get('name').updateValueAndValidity();
+            }
+        )
+    }
 }
+
